cd9814f3770718480bf68cca3734f7256ecfb41a
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-07 03:36:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,9 @@
-def my_collect (collection) 
-i = 0 
-
-collection.map { |string|
-while i < collection.length 
-yield(collection[i])
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+    i += 1
+  end
+  name_collection
 end
-}
-  
-end 
-
